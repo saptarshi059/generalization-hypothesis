bf2da3c374557d9743ed32d1fa20c15b120e515b
Saptarshi <ssengupta8@gmail.com>
2022-07-15 19:24:18 -0400

    SS

---
+++ src/sense_identification/SS_tests.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+from transformers import AutoTokenizer, AutoModel, logging
+from collections import defaultdict
+from itertools import combinations
+import pandas as pd
+import numpy as np
+import argparse
+import torch
+import os
+
+logging.set_verbosity(50)
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--dataset', type=str)
+parser.add_argument('--model_name', type=str)
+args = parser.parse_args()
+
+model_checkpoint = args.model_name
+
+tokenizer = AutoTokenizer.from_pretrained(model_checkpoint)
+model = AutoModel.from_pretrained(model_checkpoint)
+
+df = pd.read_csv(os.path.abspath(f'../../data/sense_data/{args.dataset}'))
+
+cos = torch.nn.CosineSimilarity()
+sim_scores = defaultdict(list)
+
+for word in df['word'].unique():
+    word_indices = df[df['word'] == word].index
+    for comb in list(combinations(list(range(word_indices[0], word_indices[-1]+1)), 2)):
+        indexA = comb[0]
+        indexB = comb[1]
+
+        tokenized_inputA = tokenizer(df.iloc[indexA].example, return_tensors='pt') 
+        pooler_outputA = model(**tokenized_inputA).pooler_output
+
+        tokenized_inputB = tokenizer(df.iloc[indexB].example, return_tensors='pt') 
+        pooler_outputB = model(**tokenized_inputB).pooler_output
+
+        sim_scores[(word, df.iloc[comb[0]].sense_def, df.iloc[comb[1]].sense_def)].append(cos(pooler_outputA, pooler_outputB).item())
+
+print(f'Model: {model_checkpointl}')
+
+for key, val in sim_scores.items():
+    print(key, np.round(torch.mean(torch.Tensor(val)).item(), 2))
