3de454baa01f3378f46933b5aee9e6dee76c68b3
Saptarshi <ssengupta8@gmail.com>
2022-10-11 18:18:58 -0400

    shell script to run all

--- src/answer_length_analysis/run_all.sh
+++ src/answer_length_analysis/run_all.sh
@@ -1,3 +1,4 @@
 for trained_model in $1*/ ; do
-    python noft.py --model_checkpoint trained_model --dataset $2
+    echo trained_model
+    #python noft.py --model_checkpoint trained_model --dataset $2
 done
