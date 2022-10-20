0a5f197803cd669dd4ef38bcbd98f1e219b1b2f5
Saptarshi <ssengupta8@gmail.com>
2022-10-20 17:37:22 -0400

    saving files fix in noft.py

--- src/answer_length_analysis/eval.py
+++ src/answer_length_analysis/eval.py
@@ -1,4 +1,4 @@
-from datasets import load_metric
+from evaluate import load
 import pickle5 as pickle
 import pandas as pd
 import numpy as np
@@ -10,7 +10,7 @@ parser.add_argument('--pred_file', type=str)
 parser.add_argument('--metric', default='squad' ,type=str)
 args = parser.parse_args()
 
-metric = load_metric(args.metric)
+metric = load(args.metric)
 
 s = pickle.load(open(args.pred_file, 'rb'))
 
