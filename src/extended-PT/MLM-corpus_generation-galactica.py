e3e98d42f41c40cad84e416b96387c8a086796f9
Saptarshi <ssengupta8@gmail.com>
2023-01-17 15:00:01 -0500

    changed generation prompt

--- src/extended-PT/MLM-corpus_generation-galactica.py
+++ src/extended-PT/MLM-corpus_generation-galactica.py
@@ -49,7 +49,7 @@ for tup in tqdm(most_common_entities):
     
     for i in range(args.num_of_ctx_per_ent):
         set_seed(i)
-        final_string = final_string + ' ' + generator(f'{entity}', renormalize_logits=True, do_sample=True, max_length=2048, top_p=0.9, temperature=0.9, use_cache=True)[0]['generated_text']
+        final_string = final_string + ' ' + generator(f'Title: {entity}', renormalize_logits=True, do_sample=True, max_length=2048, top_p=0.9, temperature=0.9, use_cache=True)[0]['generated_text']
     
     selected_ents_text_dict[entity] = final_string.strip()
 
