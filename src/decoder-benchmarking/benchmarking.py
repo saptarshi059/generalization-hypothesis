8387ea52981f956eb48e2735a0d9281426c364a2
Saptarshi <ssengupta8@gmail.com>
2024-03-06 14:06:18 -0500

    benchmarking updates

--- src/decoder-benchmarking/benchmarking.py
+++ src/decoder-benchmarking/benchmarking.py
@@ -43,8 +43,8 @@ class TechQA(Dataset):
         self.samples = []
         for row in tqdm(ds):
             context = row['context']
-            context_chunks = tokenizer(context, add_special_tokens=False, truncation=True, max_length=150,
-                                       stride=50, return_overflowing_tokens=True)
+            context_chunks = tokenizer(context, add_special_tokens=False, truncation=True, max_length=100,
+                                       stride=10, return_overflowing_tokens=True)
             true_spans = row['answers']['text']
             question = row['question']
 
