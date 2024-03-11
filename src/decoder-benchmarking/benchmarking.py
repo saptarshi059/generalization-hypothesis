3f065b9f313cea8c0f365c63f2539ed4fb85d5c5
Saptarshi <ssengupta8@gmail.com>
2024-03-11 17:16:10 -0400

    benchmarking updates

--- src/decoder-benchmarking/benchmarking.py
+++ src/decoder-benchmarking/benchmarking.py
@@ -164,7 +164,7 @@ if __name__ == '__main__':
                  columns=['predictions', 'reference']).to_pickle(f'{checkpoint.replace("/", "_")}_preds.pkl')
 
     count = 0
-    for (pred, ctx) in zip(predictions, dataset['test']['context']):
+    for (pred, ctx) in zip(predictions, dataset['test']['context'] if args.dataset != 'ibm/duorc' else dataset['test']['plot']):
         if pred in ctx:
             count += 1
     print(f'No. of predictions ACTUALLY (exactly) IN the entire context: {count}')
--- src/decoder-benchmarking/run_all.sh
+++ src/decoder-benchmarking/run_all.sh
@@ -1,26 +1,26 @@
 export CUDA_VISIBLE_DEVICES=0
 
-#python benchmarking.py --model_checkpoint google/gemma-7b-it --dataset Saptarshi7/covid_qa_cleaned_CS
-#python benchmarking.py --model_checkpoint BioMistral/BioMistral-7B --dataset Saptarshi7/covid_qa_cleaned_CS
+python benchmarking.py --model_checkpoint google/gemma-7b-it --dataset Saptarshi7/covid_qa_cleaned_CS
+python benchmarking.py --model_checkpoint BioMistral/BioMistral-7B --dataset Saptarshi7/covid_qa_cleaned_CS
 
-#python benchmarking.py --model_checkpoint tiiuae/falcon-7b-instruct --dataset squad
-#python benchmarking.py --model_checkpoint garage-bAInd/Platypus2-7B --dataset squad
-#python benchmarking.py --model_checkpoint google/gemma-7b-it --dataset squad
-#python benchmarking.py --model_checkpoint mistralai/Mistral-7B-Instruct-v0.2 --dataset squad
+python benchmarking.py --model_checkpoint tiiuae/falcon-7b-instruct --dataset squad
+python benchmarking.py --model_checkpoint garage-bAInd/Platypus2-7B --dataset squad
+python benchmarking.py --model_checkpoint google/gemma-7b-it --dataset squad
+python benchmarking.py --model_checkpoint mistralai/Mistral-7B-Instruct-v0.2 --dataset squad
 
 python benchmarking.py --model_checkpoint tiiuae/falcon-7b-instruct --dataset ibm/duorc
 python benchmarking.py --model_checkpoint garage-bAInd/Platypus2-7B --dataset ibm/duorc
 python benchmarking.py --model_checkpoint google/gemma-7b-it --dataset ibm/duorc
 python benchmarking.py --model_checkpoint mistralai/Mistral-7B-Instruct-v0.2 --dataset ibm/duorc
 
-#python benchmarking.py --model_checkpoint tiiuae/falcon-7b-instruct --dataset Saptarshi7/techqa-squad-style
-#python benchmarking.py --model_checkpoint garage-bAInd/Platypus2-7B --dataset Saptarshi7/techqa-squad-style
-#python benchmarking.py --model_checkpoint google/gemma-7b-it --dataset Saptarshi7/techqa-squad-style
-#python benchmarking.py --model_checkpoint mistralai/Mistral-7B-Instruct-v0.2 --dataset Saptarshi7/techqa-squad-style
-#python benchmarking.py --model_checkpoint microsoft/phi-2 --dataset Saptarshi7/techqa-squad-style
+python benchmarking.py --model_checkpoint tiiuae/falcon-7b-instruct --dataset Saptarshi7/techqa-squad-style
+python benchmarking.py --model_checkpoint garage-bAInd/Platypus2-7B --dataset Saptarshi7/techqa-squad-style
+python benchmarking.py --model_checkpoint google/gemma-7b-it --dataset Saptarshi7/techqa-squad-style
+python benchmarking.py --model_checkpoint mistralai/Mistral-7B-Instruct-v0.2 --dataset Saptarshi7/techqa-squad-style
+python benchmarking.py --model_checkpoint microsoft/phi-2 --dataset Saptarshi7/techqa-squad-style
 
-#python benchmarking.py --model_checkpoint tiiuae/falcon-7b-instruct --dataset cuad
-#python benchmarking.py --model_checkpoint garage-bAInd/Platypus2-7B --dataset cuad
-#python benchmarking.py --model_checkpoint google/gemma-7b-it --dataset cuad
-#python benchmarking.py --model_checkpoint mistralai/Mistral-7B-Instruct-v0.2 --dataset cuad
+python benchmarking.py --model_checkpoint tiiuae/falcon-7b-instruct --dataset cuad
+python benchmarking.py --model_checkpoint garage-bAInd/Platypus2-7B --dataset cuad
+python benchmarking.py --model_checkpoint google/gemma-7b-it --dataset cuad
+python benchmarking.py --model_checkpoint mistralai/Mistral-7B-Instruct-v0.2 --dataset cuad
 python benchmarking.py --model_checkpoint AdaptLLM/law-LLM --dataset cuad --max_new_tokens 20
