0c5ba12376b5acf7b5ba893c40f454862152c35a
Saptarshi <ssengupta8@gmail.com>
2024-03-19 12:35:45 -0400

    qc_identify updates

--- src/decoder_experiments/qc_identify.py
+++ src/decoder_experiments/qc_identify.py
@@ -112,18 +112,19 @@ if __name__ == '__main__':
         formatted_dataset = ChunkDataset(dataset['test'])
     dataloader = DataLoader(formatted_dataset, batch_size=args.batch_size, shuffle=False)
 
-    for batch in dataloader:
-        print(batch)
-        break
+    gold_answers = [x['text'] for x in dataset['test']['answers'][:2]]
+    print(gold_answers)
 
-    '''
     generator = pipeline('text-generation', model=checkpoint, tokenizer=tokenizer, device='cuda:0',
                          pad_token_id=tokenizer.eos_token_id, torch_dtype=torch.bfloat16)
     print(f'Model: {checkpoint} loaded...')
 
+    '''
     gold_answers = []
     for el in dataset['test']['answers']:
         gold_answers.append(el['text'] if args.dataset != 'ibm/duorc' else el)
+    '''
+    #gold_answers = [x['text'] for x in dataset['test']['answers'][:2]]
 
     print('Generating Predictions...')
     predictions = []
@@ -135,4 +136,3 @@ if __name__ == '__main__':
     pd.DataFrame(zip(predictions, gold_answers),
                  columns=['predictions', 'reference']).to_pickle(f'{checkpoint.replace("/", "_")}'
                                                                  f'_{args.dataset}_preds.pkl')
-    '''
