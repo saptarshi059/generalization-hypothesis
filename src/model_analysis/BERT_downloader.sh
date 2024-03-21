047129ba8d2db33cb8780b40599d02a0634e99d2
Saptarshi <ssengupta8@gmail.com>
2024-03-21 18:56:59 -0400

    qc_identify updates

--- src/model_analysis/BERT_downloader.sh
+++ src/model_analysis/BERT_downloader.sh
@@ -13,7 +13,7 @@ unzip "uncased_$BERT_VARIANT_STR.zip" -d "uncased_$BERT_VARIANT_STR"
 
 echo "Converting Tensorflow Checkpoint to PyTorch..."
 export BERT_BASE_DIR="`pwd`/uncased_$BERT_VARIANT_STR"
-transformers-cli convert --model_type bert \
+yes |  convert --model_type bert \
   --tf_checkpoint $BERT_BASE_DIR/bert_model.ckpt \
   --config $BERT_BASE_DIR/bert_config.json \
   --pytorch_dump_output $BERT_BASE_DIR/pytorch_model.bin
