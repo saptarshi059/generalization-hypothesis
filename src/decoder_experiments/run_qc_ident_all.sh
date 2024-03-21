73c92cc7396aaaa0a2e43ee832f2e610e1fe51ab
Saptarshi <ssengupta8@gmail.com>
2024-03-21 11:07:50 -0400

    qc_identify updates

--- src/decoder_experiments/run_qc_ident_all.sh
+++ src/decoder_experiments/run_qc_ident_all.sh
@@ -1,13 +1,4 @@
-#!/bin/sh
-
-#SBATCH --nodes=1                   # Number of nodes to request
-#SBATCH --gpus=1                    # Number of GPUs to request
-#SBATCH --cpus-per-task=4           # Number of CPUs per node to request
-#SBATCH --job-name="Decoder-Correctly_Ident" # A nice readable name of your job, to see it in the queue, instead of numbers
-#SBATCH --output=jobName.%J.out     # Store the output console text to a file called jobName.<assigned job number>.out
-#SBATCH --error=jobName.%J.err      # Store the error messages to a file called jobName.<assigned job number>.err
-#SBATCH --mail-type=FAIL,BEGIN,END  # Send an email when the job starts, ends, or fails
-#SBATCH --mail-user=saptarshi.sengupta@l3s.de # Email address to send the email to
+export CUDA_VISIBLE_DEVICES=0
 
 python qc_identify.py --dataset 'Saptarshi7/covid_qa_cleaned_CS' --model_checkpoint 'medalpaca/medalpaca-7b'
 python qc_identify.py --dataset 'Saptarshi7/covid_qa_cleaned_CS' --model_checkpoint 'tiiuae/falcon-7b-instruct'
