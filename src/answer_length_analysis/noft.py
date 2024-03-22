a5d20870dae33c51d35383da19376e0879148563
Saptarshi <ssengupta8@gmail.com>
2024-03-21 21:07:46 -0400

    qc_identify updates

--- src/answer_length_analysis/noft.py
+++ src/answer_length_analysis/noft.py
@@ -4,6 +4,7 @@
 import argparse
 import csv
 
+import torch
 from datasets import load_dataset
 from tqdm import tqdm
 from transformers import QuestionAnsweringPipeline, AutoModelForQuestionAnswering, AutoTokenizer
@@ -33,18 +34,20 @@ model = AutoModelForQuestionAnswering.from_pretrained(model_checkpoint)
 tokenizer = AutoTokenizer.from_pretrained(model_checkpoint)
 
 nlp = QuestionAnsweringPipeline(model=model, tokenizer=tokenizer, device=0)
-raw_datasets = load_dataset(args.dataset, use_auth_token=True)
+raw_datasets = load_dataset(args.dataset, token=True)
 
 gold_answers = []
 pred_answers = []
 questions = []
 
 if args.dataset == 'Saptarshi7/techqa-squad-style':
-    for record in tqdm(raw_datasets['validation']):
-        run_main()
+    with torch.no_grad():
+        for record in tqdm(raw_datasets['validation']):
+            run_main()
 elif args.dataset in ['cuad', 'ibm/duorc'] :
-    for record in tqdm(raw_datasets['test']):
-        run_main()
+    with torch.no_grad():
+        for record in tqdm(raw_datasets['test']):
+            run_main()
 
 print('Saving predictions...')
 with open(f'{model_checkpoint.replace("/", "_")}_{args.dataset.replace("/", "_")}_predictions.csv', "w") as f:
