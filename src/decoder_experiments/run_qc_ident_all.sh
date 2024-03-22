86f3e1684f118e951c69cb61e47e7a5cc1b92364
Saptarshi <ssengupta8@gmail.com>
2024-03-21 21:32:14 -0400

    qc_identify updates

--- src/decoder_experiments/run_qc_ident_all.sh
+++ src/decoder_experiments/run_qc_ident_all.sh
@@ -1,10 +1,12 @@
 export CUDA_VISIBLE_DEVICES=0
 
+: '
 python qc_identify.py --dataset 'Saptarshi7/covid_qa_cleaned_CS' --model_checkpoint 'medalpaca/medalpaca-7b'
 python qc_identify.py --dataset 'Saptarshi7/covid_qa_cleaned_CS' --model_checkpoint 'tiiuae/falcon-7b-instruct'
 python qc_identify.py --dataset 'Saptarshi7/covid_qa_cleaned_CS' --model_checkpoint 'garage-bAInd/Platypus2-7B'
 python qc_identify.py --dataset 'Saptarshi7/covid_qa_cleaned_CS' --model_checkpoint 'google/gemma-7b-it'
 python qc_identify.py --dataset 'Saptarshi7/covid_qa_cleaned_CS' --model_checkpoint 'BioMistral/BioMistral-7B'
+'
 
 python qc_identify.py --dataset 'squad' --model_checkpoint 'tiiuae/falcon-7b-instruct'
 python qc_identify.py --dataset 'squad' --model_checkpoint 'garage-bAInd/Platypus2-7B'
