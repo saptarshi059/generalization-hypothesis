c95dcb7e4badd7fd1b32009abede1835b2685d0e
Saptarshi <ssengupta8@gmail.com>
2024-03-21 20:21:24 -0400

    qc_identify updates

--- src/answer_length_analysis/noft.py
+++ src/answer_length_analysis/noft.py
@@ -57,7 +57,8 @@ class CustomDataset(Dataset):
         record = self.records[idx]
         question = record['question']
         context = record['context'] if args.dataset != 'duorc' else record['plot']
-        inputs = tokenizer(question, context, return_tensors="pt", max_length=384, stride=128, padding="max_length")
+        inputs = tokenizer(question, context, return_tensors="pt", max_length=384, stride=128, padding="max_length",
+                           truncation="only_second" if pad_on_right else "only_first")
         return inputs
 
 
