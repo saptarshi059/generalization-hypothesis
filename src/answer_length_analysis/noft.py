3d3f0e6abd5f889a2716d6166aacb51cd7cc5577
Saptarshi <ssengupta8@gmail.com>
2024-03-23 15:39:20 -0400

    qc_identify updates

--- src/answer_length_analysis/noft.py
+++ src/answer_length_analysis/noft.py
@@ -53,8 +53,11 @@ dataloader = DataLoader(dataset, batch_size=args.batch_size, shuffle=False)
 
 with torch.no_grad():
     for questions, contexts in tqdm(dataloader):
-        predictions = nlp(question=list(questions), context=list(contexts), handle_impossible_answer=True)
-        print(predictions)
+        try:
+            predictions = nlp(question=list(questions), context=list(contexts), handle_impossible_answer=True)
+            print(predictions)
+        except:
+            print(list(questions))
         '''
         try:
             pred_answers.append(nlp(question=batch['question'], context=batch['context'],
