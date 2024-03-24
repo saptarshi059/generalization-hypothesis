b24dfa77482c090a08f1b97fb8b258193f5d649a
Saptarshi <ssengupta8@gmail.com>
2024-03-24 15:31:14 -0400

    qc_identify updates

--- src/decoder_experiments/eval_qc_all.sh
+++ src/decoder_experiments/eval_qc_all.sh
@@ -6,5 +6,5 @@ for prediction_file in $files;
 do
   echo "$prediction_file"
   formatted_file_name=$(echo "$prediction_file" | sed 's/\.\///')
-  python qc_ident_eval.py --pred_file "$formatted_file_name"
+  python qc_ident_eval.py --prediction_file "$formatted_file_name"
 done
