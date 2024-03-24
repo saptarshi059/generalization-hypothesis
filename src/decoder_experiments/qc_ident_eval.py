f257d244da5dcff23ab665b41693c7f6a5663e53
Saptarshi <ssengupta8@gmail.com>
2024-03-24 16:00:30 -0400

    qc_identify updates

--- src/decoder_experiments/qc_ident_eval.py
+++ src/decoder_experiments/qc_ident_eval.py
@@ -23,19 +23,11 @@ if __name__ == '__main__':
         prediction = row.prediction.split(row.prompt)[1]
         if re.search(re.escape(row.context), prediction):
             correctly_identified_contexts += 1
-        if row.question in prediction:
+        if re.search(re.escape(row.question), prediction):
             correctly_identified_questions += 1
-        if row.context in prediction and row.question in prediction:
+        if re.search(re.escape(row.context), prediction) and re.search(re.escape(row.question), prediction):
             correctly_identified_both += 1
 
-        '''
-        if re.search(row.context, prediction):
-            correctly_identified_contexts += 1
-        if re.search(row.question, prediction):
-            correctly_identified_questions += 1
-        if re.search(row.context, prediction) and re.search(row.question, prediction):
-            correctly_identified_both += 1
-        '''
     print(f'Correctly identified Contexts: {np.round(correctly_identified_contexts/total_samples, 2)}%')
     print(f'Correctly identified Questions: {np.round(correctly_identified_questions / total_samples, 2)}%')
     print(f'Correctly identified Both: {np.round(correctly_identified_both / total_samples, 2)}%')
