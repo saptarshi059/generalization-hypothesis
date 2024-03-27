75d48f177254799e8a61192b864978f8f159f7f0
Saptarshi <ssengupta8@gmail.com>
2024-03-27 18:55:19 -0400

    qc_identify updates

---
+++ src/task+text_emb/my_task_emb.sh
@@ -0,0 +1,29 @@
+export SEED_ID=42
+export DATA_DIR="../../data/json_data/squad"
+export MODEL_TYPE="bert"
+export VERSION_2_WITH_NEGATIVE="False"
+export USE_LABELS=True # set to False to sample from the model's predictive distribution
+export CACHE_DIR="../../data/json_data/squad"
+export MODEL_NAME_OR_PATH="deepset/bert-base-uncased-squad2"
+# we start from a fine-tuned task-specific BERT so no need for further fine-tuning
+export FURTHER_FINETUNE_CLASSIFIER=False
+export FURTHER_FINETUNE_FEATURE_EXTRACTOR=False
+export OUTPUT_DIR="../../data/json_data/squad"
+
+python ./run_taskemb_QA.py \
+    --model_type ${MODEL_TYPE} \
+    --model_name_or_path ${MODEL_NAME_OR_PATH} \
+    --use_labels ${USE_LABELS} \
+    --do_lower_case \
+    --finetune_classifier ${FURTHER_FINETUNE_CLASSIFIER} \
+    --finetune_feature_extractor ${FURTHER_FINETUNE_FEATURE_EXTRACTOR} \
+    --train_file ${DATA_DIR}/train.json \
+    --version_2_with_negative ${VERSION_2_WITH_NEGATIVE} \
+    --max_seq_length 384 \
+    --batch_size=12  \
+    --learning_rate 3e-5 \
+    --num_epochs 1 \
+    --doc_stride 128 \
+    --output_dir ${OUTPUT_DIR} \
+    --cache_dir ${CACHE_DIR} \
+    --seed ${SEED_ID}
--- src/task+text_emb/my_text_emb_run.sh
+++ src/task+text_emb/my_text_emb_run.sh
@@ -1,23 +1,38 @@
-export CUDA_VISIBLE_DEVICES=3
-export SEED_ID=42
-export DATA_DIR="../../data/json_data/squad"
-export MODEL_TYPE='bert'
-export MODEL_NAME_OR_PATH='bert-base-uncased'
-export VERSION_2_WITH_NEGATIVE='True'
-export CACHE_DIR='../../data/json_data/squad/'
-export OUTPUT_DIR='../../data/json_data/squad/'
+JSON_DATASET_PATHS=("../../data/json_data/squad"
+                    "../../data/json_data/covidqa"
+                    "../../data/json_data/techqa"
+                    "../../data/json_data/duorc"
+                    "../../data/json_data/cuad")
 
-python ./run_textemb_QA.py \
+for dataset_path in "${JSON_DATASET_PATHS[@]}"
+do
+  export CUDA_VISIBLE_DEVICES=3
+  export SEED_ID=42
+  export DATA_DIR=$dataset_path
+  export CACHE_DIR=$dataset_path
+  export OUTPUT_DIR=$dataset_path
+  export MODEL_TYPE='bert'
+  export MODEL_NAME_OR_PATH='bert-base-uncased'
+
+  if [[ $dataset_path == *"squad"* ]] || [[ $dataset_path == *"covidqa"* ]]; then
+    negative_flag='False'
+  else
+    negative_flag='True'
+  fi
+  export VERSION_2_WITH_NEGATIVE=$negative_flag
+
+  python ./run_textemb_QA.py \
     --model_type ${MODEL_TYPE} \
     --model_name_or_path ${MODEL_NAME_OR_PATH} \
     --do_lower_case \
     --version_2_with_negative ${VERSION_2_WITH_NEGATIVE} \
-    --train_file ${DATA_DIR}/train.json \
+    --train_file "${DATA_DIR}"/train.json \
     --max_seq_length 384 \
     --per_gpu_train_batch_size=12  \
     --num_train_epochs 1 \
     --doc_stride 128 \
-    --output_dir ${OUTPUT_DIR} \
-    --cache_dir ${CACHE_DIR} \
-    --seed ${SEED_ID} \
+    --output_dir "${OUTPUT_DIR}" \
+    --cache_dir "${CACHE_DIR}" \
+    --seed "${SEED_ID}" \
     --overwrite_output_dir
+done
