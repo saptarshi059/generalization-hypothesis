325f0a4498e7973eeaa3d5667acbc5ab48aed6e2
Saptarshi <ssengupta8@gmail.com>
2024-04-03 19:42:38 -0400

    spearman

---
+++ src/task+text_emb/compute_sim.py
@@ -0,0 +1,16 @@
+import argparse
+import numpy as np
+import torch
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--embedding1_file', type=str)
+    parser.add_argument('--embedding2_file', type=str)
+    args = parser.parse_args()
+
+    vector1 = torch.from_numpy(np.load(args.embedding1_file).reshape(-1))
+    vector2 = torch.from_numpy(np.load(args.embedding2_file).reshape(-1))
+
+    cos = torch.nn.CosineSimilarity()
+    print(f'Cosine Similarity between {args.embedding1_file} and '
+          f'{args.embedding1_file}: {cos(vector1, vector2).item()}')
