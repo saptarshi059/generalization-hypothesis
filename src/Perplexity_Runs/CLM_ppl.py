13811908363a59a9466fd28ec6c524c83031e4d6
Saptarshi <ssengupta8@gmail.com>
2024-04-04 19:01:08 -0400

    PPL

--- src/Perplexity_Runs/CLM_ppl.py
+++ src/Perplexity_Runs/CLM_ppl.py
@@ -21,7 +21,7 @@ class TextDataset(Dataset):
         self.attention_mask = []
         for sample in tqdm(texts):
             tokenized_chunks = tokenizer(sample, return_tensors="pt", padding='max_length', truncation=True,
-                                         max_length=max_length, stride=512, return_overflowing_tokens=True)
+                                         max_length=max_length, stride=64, return_overflowing_tokens=True)
             self.input_ids.extend(tokenized_chunks['input_ids'])
             self.attention_mask.extend(tokenized_chunks['attention_mask'])
 
@@ -38,7 +38,7 @@ if __name__ == '__main__':
     parser.add_argument('--corpus_file', default="Saptarshi7-covid_qa_cleaned_CS_for_PPL_eval.csv",
                         type=str)
     parser.add_argument('--random_state', default=42, type=int)
-    parser.add_argument('--batch_size', default=8, type=int)
+    parser.add_argument('--batch_size', default=4, type=int)
     args = parser.parse_args()
 
     # Set random seeds
@@ -59,7 +59,7 @@ if __name__ == '__main__':
     test_dataset = load_dataset("csv", data_files=args.corpus_file, split='train')
     texts = test_dataset["text"]
 
-    max_length = 2048  # Otherwise it's freaking out on other models. - Falcon fine, others not.
+    max_length = 1024  # Otherwise it's freaking out on other models. - Falcon fine, others not.
 
     dataset = TextDataset(texts)
     dataloader = DataLoader(dataset, batch_size=batch_size, worker_init_fn=seed_worker, generator=g, shuffle=False)
