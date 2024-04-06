7ba979dd337a729afd55860537c27e61c3fc54e4
Saptarshi <ssengupta8@gmail.com>
2024-04-05 20:50:35 -0400

    PPL

--- src/common_terms_freq/convert_to_csv_corpus.py
+++ src/common_terms_freq/convert_to_csv_corpus.py
@@ -2,17 +2,20 @@ from datasets import load_dataset
 import pandas as pd
 import argparse
 
-parser = argparse.ArgumentParser()
-parser.add_argument('--dataset', default="Saptarshi7/covid_qa_cleaned_CS", type=str)
-args = parser.parse_args()
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--dataset', default="Saptarshi7/covid_qa_cleaned_CS", type=str)
+    parser.add_argument('--split', default='train', type=str)
+    args = parser.parse_args()
 
-print('Reading dataset...')
-if args.dataset == 'duorc':
-  dataset = load_dataset('duorc', 'SelfRC')
-  total = list(set(dataset['train']['plot'])) + list(set(dataset['train']['question']))
-else:
-  dataset = load_dataset(args.dataset, token=True)
-  total = list(set(dataset['train']['context'])) + list(set(dataset['train']['question']))
+    print('Reading dataset...')
+    if args.dataset == 'duorc':
+        dataset = load_dataset('duorc', 'SelfRC')
+        total = list(set(dataset[args.split]['plot'])) + list(set(dataset[args.split]['question']))
+    else:
+        dataset = load_dataset(args.dataset, token=True)
+        total = list(set(dataset[args.split]['context'])) + list(set(dataset[args.split]['question']))
 
-pd.DataFrame(zip(list(range(len(total))), total), columns=['ent', 'text']).to_csv(f"{args.dataset.replace('/','-')}_for_PPL_eval.csv", index=False)
-print('Saving dataset...')
+    pd.DataFrame(zip(list(range(len(total))), total), columns=['ent', 'text']).to_csv(
+        f"{args.dataset.replace('/', '-')}_for_PPL_eval.csv", index=False)
+    print('Saving dataset...')
