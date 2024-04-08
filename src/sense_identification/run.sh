6452df6daa2cae2bf7f852b8d4632e24ada6d8c7
Saptarshi <ssengupta8@gmail.com>
2024-04-08 19:21:49 -0400

    semantic similarity

--- src/sense_identification/run.sh
+++ src/sense_identification/run.sh
@@ -8,7 +8,11 @@ python SS_tests.py --dataset sense_data-cuad.csv --model_name nlpaueb/legal-bert
 python SS_tests.py --dataset sense_data-techqa.csv --model_name allenai/scibert_scivocab_uncased
 '
 
-MODELS=('mistralai/Mistral-7B-Instruct-v0.2'
+MODELS=('bert-base-uncased'
+        'roberta-base'
+        'tiiuae/falcon-7b-instruct'
+        'garage-bAInd/Platypus2-7B'
+        'mistralai/Mistral-7B-Instruct-v0.2'
         'google/gemma-7b-it')
 
 DATASETS=$(ls ../../data/sense_data/*.csv)
