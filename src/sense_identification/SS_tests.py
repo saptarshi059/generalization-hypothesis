8e1b5558f786b152c7ed0e94e9f61747c975ec6d
Saptarshi <ssengupta8@gmail.com>
2024-04-08 17:49:31 -0400

    semantic similarity

--- src/sense_identification/SS_tests.py
+++ src/sense_identification/SS_tests.py
@@ -128,6 +128,8 @@ else:
                 entity_embeddingB = contextualized_embeddingsB[0][
                     tokenized_inputB['input_ids'].tolist()[0].index(wordB_vocab_idx)]
 
+                print(entity_embeddingA, entity_embeddingB)
+
                 sim_scores[(word, df.iloc[indexA].sense_def, df.iloc[indexB].sense_def)].append( \
                     cos(entity_embeddingA, entity_embeddingB).item())
 
