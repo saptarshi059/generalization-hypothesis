79dfe2b7c259514624c306227f5015d0b71ce1a6
Saptarshi <ssengupta8@gmail.com>
2024-04-08 14:38:20 -0400

    PPL

--- src/sense_identification/run.sh
+++ src/sense_identification/run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-:'
+: '
 python SS_tests.py --dataset sense_data-covidqa.csv --model_name dmis-lab/biobert-base-cased-v1.2
 python SS_tests.py --dataset sense_data-covidqa.csv --model_name allenai/scibert_scivocab_uncased
 python SS_tests.py --dataset sense_data-cuad.csv --model_name ProsusAI/finbert
@@ -13,12 +13,12 @@ MODELS=('tiiuae/falcon-7b-instruct'
         'google/gemma-7b-it'
         'mistralai/Mistral-7B-Instruct-v0.2')
 
-DATASETS=$(ls ../..data/sense_data/*.csv)
+DATASETS=$(ls ../../data/sense_data/*.csv)
 
 for ds in $DATASETS
 do
    for model in "${MODELS[@]}"
    do
-     python SS_tests --dataset "$ds" --model_name "$model"
+     python SS_tests.py --dataset "$ds" --model_name "$model"
    done
 done
