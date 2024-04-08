55cca38aba11d833d584551ec8d5580a410bf249
Saptarshi <ssengupta8@gmail.com>
2024-04-08 14:35:10 -0400

    PPL

--- src/sense_identification/run.sh
+++ src/sense_identification/run.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 :'
 python SS_tests.py --dataset sense_data-covidqa.csv --model_name dmis-lab/biobert-base-cased-v1.2
 python SS_tests.py --dataset sense_data-covidqa.csv --model_name allenai/scibert_scivocab_uncased
