ff04625e35756c11b553e75e0b468793a4c02a9f
Saptarshi <ssengupta8@gmail.com>
2024-04-08 20:19:52 -0400

    semantic similarity

--- src/sense_identification/SS_tests.py
+++ src/sense_identification/SS_tests.py
@@ -192,4 +192,5 @@ for key, val in sim_scores.items():
 
 pd.DataFrame(final_list,
              columns=['word', 'sense1', 'sense2', 'score']).to_csv(f'{model_checkpoint.replace("/", "_")}'
-                                                                   f'_{args.dataset}')
+                                                                   f'_{args.dataset.replace("../../data/sense_data",
+                                                                                            "_")}')
